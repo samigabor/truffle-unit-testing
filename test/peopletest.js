c00f00b5789ca5aa3f4a0dc1097e63cd3d1ab122
Samoila Gabor <samoila.gabor@iquestgroup.com>
2020-11-22 09:51:13 +0200

    test person creation

--- test/peopletest.js
+++ test/peopletest.js
@@ -3,10 +3,31 @@ const truffleAssert = require('truffle-assertions');
 
 contract('People', async () => {
   it('should not create a person older than 150 years', async () => {
-    const instance = await People.deployed();
+    const contractInstance = await People.deployed();
     await truffleAssert.fails(
-      instance.createPerson('Sam', 151, 180, {value: web3.utils.toWei('1', 'ether')}),
+      contractInstance.createPerson('Sam', 151, 180, {value: web3.utils.toWei('1', 'ether')}),
       truffleAssert.ErrorType.REVERT
-    )
-  })
-})
+    );
+  });
+
+  it('should not create a person without payment', async () => {
+    const contractInstance = await People.deployed();
+    await truffleAssert.fails(
+      contractInstance.createPerson('Samy', 40, 180, { value: 1000 }),
+      truffleAssert.ErrorType.REVERT
+    );
+  });
+
+  it('should set senior status correctly', async () => {
+    const contractInstance = await People.deployed();
+    await contractInstance.createPerson('Sammy', 70, 170, {value: web3.utils.toWei('1', 'ether')});
+    const person = await contractInstance.getPerson();
+    assert.equal(person[3], true);
+  });
+
+  it('should set the age correctly', async () => {
+    const contractInstance = await People.deployed();
+    const person = await contractInstance.getPerson();
+    assert.equal(person[1], 70);
+  });
+});
